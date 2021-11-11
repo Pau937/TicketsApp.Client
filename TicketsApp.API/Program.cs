a1ccdebecf11a0290bb958c16b2abf697f92cfb4
Paweł Kolbiarz <pawel.kolbiarz@gmail.com>
2021-11-11 22:52:49 +0100

    update to net 6

--- TicketsApp.API/Program.cs
+++ TicketsApp.API/Program.cs
@@ -1,20 +1,45 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TicketsApp.API.Middlewares;
+using TicketsApp.Core.Interfaces;
+using TicketsApp.Core.Services;
+using TicketsApp.Core.Services.Interfaces;
+using TicketsApp.DataAccess.Data;
+using TicketsApp.DataAccess.Repositories;
 
-namespace TicketsApp.API
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+var allowSpecificOriginsCors = "AllowSpecificOrigins";
+
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: allowSpecificOriginsCors, builder => 
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        builder.WithOrigins("http://localhost:3000");
+    });
+});
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddScoped(typeof(IAsyncRepository<>), typeof(EFCoreRepository<>));
+builder.Services.AddScoped<ITicketService, TicketService>();
+
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ForbiddenBrowsersMiddleware>();
+
+app.UseRouting();
+app.UseCors(allowSpecificOriginsCors);
+app.MapControllers();
+
+app.Run();
